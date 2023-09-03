d368b17f2e6a51d8404f18a275588dd27bc66f4b
ksquarekumar <ksquarekumar@gmail.com>
2023-09-03 19:23:29 +0000

    index on tooling: f0cfa94 chore: sync with upstream

--- whisper_stream/core/helpers/batching.py
+++ whisper_stream/core/helpers/batching.py
@@ -54,10 +54,10 @@ def preprocess_batch_adaptive(
     target_sampling_rate: int = 16000,
     strategy: BatchedTaskProcessingStrategy = "smallest",
     audio_preprocessor: Callable[
-        ..., tuple[AudioFilesDType, int]
+        ..., tuple[list[AudioFilesDType], int]
     ] = preprocess_inputs_with_ffmpeg,
     fallback_resampler: Callable[
-        ..., tuple[AudioFilesDType, float]
+        ..., tuple[list[AudioFilesDType], float]
     ] = resample_inputs_with_fn,
     fallback_resampler_backend: Callable[..., AudioFilesDType] = DEFAULT_RESAMPLER_FN,
     parallel_backend: Parallel = DEFAULT_PARALLEL_BACKEND,
@@ -81,90 +81,115 @@ def preprocess_batch_adaptive(
     else:
         ratio = 1.0
 
-    asc_sorted_vector_lengths = np.array([len(vec) for vec in _inputs])
-    asc_sorted_inputs_indices = np.argsort(asc_sorted_vector_lengths)
-    asc_sorted_inputs = _inputs[asc_sorted_inputs_indices]
+    cutoff_length: int = int(chunk_length_s * target_sampling_rate)
 
-    asc_sorted_split_at_index: int = int(
-        np.searchsorted(
-            asc_sorted_vector_lengths[asc_sorted_inputs_indices],
-            int(chunk_length_s * target_sampling_rate),
-            side="right",
+    # For case of 1
+    if len(_inputs) <= 1:
+        if _inputs[0].shape[0] > cutoff_length:
+            yield from tasks_callable_partial_mapping["largest"].task_callable(
+                **{
+                    "inputs": _inputs,
+                    "chunk_length_s": chunk_length_s,
+                    "stride_length_s": stride_length_s,
+                    "batch_size": batch_size,
+                    "target_sampling_rate": target_sampling_rate,
+                    "ratio": ratio,
+                    **tasks_callable_partial_mapping["largest"].task_kwargs,
+                }
+            )
+        else:
+            yield from tasks_callable_partial_mapping["smallest"].task_callable(
+                **{
+                    "inputs": _inputs,
+                    "batch_size": batch_size,
+                    "target_sampling_rate": target_sampling_rate,
+                    **tasks_callable_partial_mapping["smallest"].task_kwargs,
+                }
+            )
+    else:
+        # For more than 1
+        asc_sorted_vector_lengths = np.array([len(vec) for vec in _inputs])
+        asc_sorted_inputs_indices = np.argsort(asc_sorted_vector_lengths)
+        asc_sorted_inputs = np.array(_inputs, dtype=object)[asc_sorted_inputs_indices]
+
+        asc_sorted_split_at_index: int = int(
+            np.searchsorted(
+                asc_sorted_vector_lengths[asc_sorted_inputs_indices],
+                cutoff_length,
+                side="right",
+            )
         )
-    )
 
-    unchunkable_inputs: AudioFilesDType = asc_sorted_inputs[:asc_sorted_split_at_index]
-    chunkable_inputs: AudioFilesDType = asc_sorted_inputs[asc_sorted_split_at_index:]
+        unchunkable_inputs: list[AudioFilesDType] = list(asc_sorted_inputs[:asc_sorted_split_at_index])
+        chunkable_inputs: list[AudioFilesDType] = list(asc_sorted_inputs[asc_sorted_split_at_index:])
 
-    if logger is not None:
-        logger.debug(
-            "feature_extractor(prepocess_batch)",
-            inputs_shapes=[inputs.shape for inputs in _inputs],
-            unchunkable_inputs=[
-                unchunkable_input.shape for unchunkable_input in unchunkable_inputs
-            ],
-            chunkable_inputs=[
-                chunkable_input.shape for chunkable_input in chunkable_inputs
-            ],
-            target_sampling_rate=target_sampling_rate,
-            ratio=ratio,
+        if logger is not None:
+            logger.debug(
+                "feature_extractor(prepocess_batch)",
+                inputs_shapes=[inputs.shape for inputs in _inputs],
+                unchunkable_inputs=[
+                    unchunkable_input.shape for unchunkable_input in unchunkable_inputs
+                ],
+                chunkable_inputs=[
+                    chunkable_input.shape for chunkable_input in chunkable_inputs
+                ],
+                target_sampling_rate=target_sampling_rate,
+                ratio=ratio,
+            )
+
+        task_order: list[BatchedTaskProcessingStrategy] = (
+            ["smallest", "largest"] if strategy == "smallest" else ["largest", "smallest"]
         )
 
-    task_order: list[BatchedTaskProcessingStrategy] = (
-        ["smallest", "largest"] if strategy == "smallest" else ["largest", "smallest"]
-    )
+        tasks_map: dict[BatchedTaskProcessingStrategy, BatchPreProcessorTasksMapping] = {
+            "smallest": BatchPreProcessorTasksMapping(
+                task_callable=tasks_callable_partial_mapping["smallest"].task_callable,
+                task_kwargs={
+                    "inputs": unchunkable_inputs,
+                    "batch_size": batch_size,
+                    "target_sampling_rate": target_sampling_rate,
+                    **tasks_callable_partial_mapping["smallest"].task_kwargs,
+                },
+            ),
+            "largest": BatchPreProcessorTasksMapping(
+                task_callable=tasks_callable_partial_mapping["largest"].task_callable,
+                task_kwargs={
+                    "inputs": chunkable_inputs,
+                    "chunk_length_s": chunk_length_s,
+                    "stride_length_s": stride_length_s,
+                    "batch_size": batch_size,
+                    "target_sampling_rate": target_sampling_rate,
+                    "ratio": ratio,
+                    **tasks_callable_partial_mapping["largest"].task_kwargs,
+                },
+            ),
+        }
 
-    tasks_map: dict[BatchedTaskProcessingStrategy, BatchPreProcessorTasksMapping] = {
-        "smallest": BatchPreProcessorTasksMapping(
-            task_callable=tasks_callable_partial_mapping["smallest"].task_callable,
-            task_kwargs={
-                "inputs": unchunkable_inputs,
-                "batch_size": batch_size,
-                "target_sampling_rate": target_sampling_rate,
-                **tasks_callable_partial_mapping["smallest"].task_kwargs,
-            },
-        ),
-        "largest": BatchPreProcessorTasksMapping(
-            task_callable=tasks_callable_partial_mapping["largest"].task_callable,
-            task_kwargs={
-                "inputs": chunkable_inputs,
-                "chunk_length_s": chunk_length_s,
-                "stride_length_s": stride_length_s,
-                "batch_size": batch_size,
-                "target_sampling_rate": target_sampling_rate,
-                "ratio": ratio,
-                **tasks_callable_partial_mapping["largest"].task_kwargs,
-            },
-        ),
-    }
-
-    for task in task_order:
-        if len(tasks_map[task].task_kwargs["inputs"]) > 0:
-            task_callable = tasks_map[task].task_callable
-            task_kwargs = tasks_map[task].task_kwargs
-            yield from task_callable(**task_kwargs)
+        for task in task_order:
+            if len(tasks_map[task].task_kwargs["inputs"]) > 0:
+                task_callable = tasks_map[task].task_callable
+                task_kwargs = tasks_map[task].task_kwargs
+                yield from task_callable(**task_kwargs)
 
 
 def split_array_on_primary_axis(
-    arr: np.ndarray[Any, Any], batch_size: int, logger: BoundLogger | None = None
-) -> list[np.ndarray[Any, Any]]:
+    arr: list[AudioFilesDType], batch_size: int, logger: BoundLogger | None = None
+) -> list[list[AudioFilesDType]]:
     num_batches: int = math.ceil(len(arr) / batch_size)
     batch_length: int = len(arr) // num_batches
-    splits: list[np.ndarray[Any, Any]] = []
-    for i in range(0, num_batches, batch_length):
-        splits.append(np.array([*arr[i : i + batch_length]]))
+    splits: list[list[AudioFilesDType]] = [arr[i:(i+batch_length)] for i in range(0, num_batches, batch_length)]
     if logger is not None:
         logger.debug(
             "feature_extractor(preprocess_batches_for_unchunkable):split_array_on_primary_axis",
             unchunkable_inputs=[subarr.shape for subarr in arr],
             batch_size=batch_size,
-            splits=[split.shape for split in splits],
+            splits=[f"len={len(split)}" for split in splits],
         )
     return splits
 
 
 def pregenerate_batching_info_for_chunkable_audio(
-    chunkable_inputs: AudioFilesDType,
+    chunkable_inputs: list[AudioFilesDType],
     chunk_length_s: float,
     stride_length_s: float | None,
     batch_size: int,
@@ -214,16 +239,15 @@ def pregenerate_batching_info_for_chunkable_audio(
 
 
 def iterate_batch_for_chunkable_audio(
-    inputs: AudioFilesDType,
+    single_input: AudioFilesDType,
     chunk_len: int,
     stride_left: int,
     stride_right: int,
     batch_size: int,
     logger: BoundLogger | None = None,
 ) -> ChunkedIterationsGenerator:
-    inputs_len: int = inputs.shape[
-        0
-    ]  # Size of 1-D input / length of 1-channel audio array
+    inputs_len: int = single_input.shape[0]
+    # Size of 1-D input / length of 1-channel audio array
     step: int = chunk_len - stride_left - stride_right
 
     all_chunks_start_idx: VecSignedInt = np.arange(0, inputs_len, step)
@@ -235,7 +259,7 @@ def iterate_batch_for_chunkable_audio(
     if logger is not None:
         logger.debug(
             "feature_extractor(iterate_batch_for_chunkable):gather",
-            shape=inputs.shape[0],
+            shape=single_input.shape,
             num_batches=num_batches,
             chunk_len=chunk_len,
             stride_left=stride_left,
@@ -250,7 +274,7 @@ def iterate_batch_for_chunkable_audio(
         chunk_end_idx: VecSignedInt = chunk_start_idx + chunk_len
 
         chunks: list[AudioFilesDType] = [
-            inputs[chunk_start:chunk_end]
+            single_input[chunk_start:chunk_end]
             for chunk_start, chunk_end in zip(chunk_start_idx, chunk_end_idx)
         ]
 
